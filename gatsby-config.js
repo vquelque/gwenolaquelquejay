3b7f3cc30fe62dfd12d8d6f17f95774d06b5e75c
vquelque <valquelque@gmail.com>
2022-01-09 15:48:55 +0100

    Fix favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,12 +28,10 @@ module.exports = {
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon_package/favicon.ico`, // This path is relative to the root of the site.
-      },
+        icon: `./src/images/favicon_package/favicon-32x32.png`,
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
+    },]
 }
