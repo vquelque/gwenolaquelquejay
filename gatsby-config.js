7739983477bfd479f1db240fd5702277ceac33fe
vquelque <valquelque@gmail.com>
2022-01-16 21:53:26 +0100

    add g analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,11 +4,27 @@ module.exports = {
     description: `Prendre rendez-vous avec Gwenola Quelquejay, praticienne Psycho-Bio-Acupressure et hypnothérapeute à Paris ou Fontainebleau.`,
     author: `Gwenola Quelquejay`,
     siteUrl: `https://gwenolaquelquejay.fr`, //no trailing slash
-    keywords: `acupressure, hypnose, paris, fontainebleau, médecine douce, psycho-bio-acupressure, pba, delatte, émotionnel`
+    keywords: `acupressure, hypnose, paris, fontainebleau, médecine douce, psycho-bio-acupressure, pba, delatte, émotionnel`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-EK6YEPZDP5", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-mailgo`, 
+    `gatsby-plugin-mailgo`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -30,9 +46,10 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         icon: `./src/images/favicon_package/favicon-32x32.png`,
+      },
+      // this (optional) plugin enables Progressive Web App + Offline functionality
+      // To learn more, visit: https://gatsby.dev/offline
+      // `gatsby-plugin-offline`,
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-    },]
+  ],
 }
