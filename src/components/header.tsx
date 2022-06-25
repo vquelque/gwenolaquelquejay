5675b371c9d09ee75550c06d5549e86e78834d6d
vquelque <valquelque@gmail.com>
2022-06-25 16:14:23 +0200

    remove unused dependency

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 import Logo from "../images/logo.svg";
 
 const Header = ({ siteTitle }) => (
